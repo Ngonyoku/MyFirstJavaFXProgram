54981b8a34bd599b70e966a7f1bd8c980cd06630
Ngonyoku <ngonyoku001@gmail.com>
2020-12-15 17:04:37 +0300

    Creating a basic window

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,17 +1,23 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("My New Window");
-        stage.show();//
+        stage.setTitle("NGONYOKU");//Set The Title of the Window
+
+        StackPane layout = new StackPane();//Set the layout
+        Scene scene = new Scene(layout, 600, 500); //Initialize the Scene
+        stage.setScene(scene);//Set the Scene
+        stage.show();//Display Everything
     }
 
     public static void main(String[] args) {
-        launch(args);//Sets Up everything
+        launch(args);//Sets Up everything and then Calls the stage() method
     }
 }
