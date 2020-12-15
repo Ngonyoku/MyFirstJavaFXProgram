f55455ef0c1c176186bb7b615e6b2f07eb9ad192
Ngonyoku <ngonyoku001@gmail.com>
2020-12-15 18:46:56 +0300

    Dialog Box Created

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -2,6 +2,7 @@ package sample;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -9,12 +10,21 @@ public class Main extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        stage.setTitle("NGONYOKU");//Set The Title of the Window
+        Button openDialogBtn = new Button("Open Dialog");
+        openDialogBtn.setOnAction(
+                event -> {
+                    AlertDialogBox dialogBox = new AlertDialogBox("My Window", "Just some message");
+                    dialogBox.setPositiveButton("YES", e -> System.out.println("I have been Clicked!"));
+                    dialogBox.show();
+                }
+        );
 
-        StackPane layout = new StackPane();//Set the layout
-        Scene scene = new Scene(layout, 600, 500); //Initialize the Scene
-        stage.setScene(scene);//Set the Scene
-        stage.show();//Display Everything
+        StackPane layout = new StackPane();
+        layout.getChildren().add(openDialogBtn);
+
+        Scene scene = new Scene(layout, 800, 600);
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
