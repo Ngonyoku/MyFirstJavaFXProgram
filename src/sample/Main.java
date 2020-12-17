3d6ef59bbce0986146393f8c0ab1d025a26c8be6
Ngonyoku <ngonyoku001@gmail.com>
2020-12-17 17:12:03 +0300

    Closing a Window

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,44 +1,47 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-    Scene scene1, scene2;
-    HBox l1;
-    VBox l2;
+    Scene scene;
+    Stage window;
 
     @Override
     public void start(Stage stage) throws Exception {
-        l1 = new HBox(10);
-        l2 = new VBox(10);
+        window = stage;
+        window.setOnCloseRequest(e -> {
+            e.consume();
+            close();
+        });
+        Button closeBtn = new Button("CLOSE");
+        closeBtn.setOnAction(e -> close());
 
-        Label label1 = new Label();
-        Label label2 = new Label();
-        label1.setText("First Scene");
-        label2.setText("Second Scene");
-        Button next = new Button("NEXT");
-        Button prev = new Button("PREV");
+        VBox layout = new VBox(10);
+        layout.setAlignment(Pos.CENTER);
+        layout.getChildren().addAll(new Label("Press The Button to close the Window"), closeBtn);
 
-        l1.getChildren().addAll(label1, next);
-        l2.getChildren().addAll(label2, prev);
-        scene1 = new Scene(l1, 250, 250);
-        scene2 = new Scene(l2, 300, 300);
-        next.setOnAction(
-                event -> stage.setScene(scene2)
-        );
-        prev.setOnAction(
-                event -> stage.setScene(scene1)
-        );
+        scene = new Scene(layout, 500, 550);
+        window.setScene(scene);
+        window.show();
+    }
 
-        stage.setScene(scene1);
-        stage.show();
+    private void close() {
+        AlertDialogBox closeRequest = new AlertDialogBox();
+        closeRequest.setWindowTitle("Dangerous Operation");
+        closeRequest.setMessage("Are you sure you want to Exit this Application?");
+        closeRequest.setPositiveButton("YES", event -> {
+            System.out.println("File saved Successfully");
+            closeRequest.closeDialog();
+            window.close();
+        });
+        closeRequest.setNegativeButton("NO");
+        closeRequest.show();
     }
 
     public static void main(String[] args) {
